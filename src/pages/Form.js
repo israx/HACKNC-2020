31c6800e2cdeee91e7d21cf03def34e88074eab4
israx <70438514+israx@users.noreply.github.com>
2020-10-18 00:09:25 -0500

    form update

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,129 +1,124 @@
-import React from "react";
+import React, { useState } from "react";
 // import {Alert} from "react-native";
 // import Checkbox from "./CheckBox";
 
-class Form extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        name: '', 
-        email: '',
-        subject: '',
-        // subjects: {
-        //     {id: 1, value: "Mathematics", isChecked: false},
-        //     {id: 2, value: "Science", isChecked: false},
+function Form() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [avail, setAvail] = useState("");
+  const [tutorOb, setTutorOb] = useState({});
+  const [newTutors, setNewTutors] = useState([]);
+  const [on, setOn] = useState(true);
 
-        // }
-        // avail: '',
-        // available: [false, false, false, false, false, false, false],
-    };
+  //   const object = { n: "whatever" };
+  //   const { n } = object;
+  //   console.log(n);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    if (name === "name") {
+      setName(value);
+      setTutorOb((prev) => ({ ...prev, [name]: value }));
+      setOn(true);
     }
-
-    handleNameChange = (event) => {
-        this.setState({
-            name: event.target.value
-        })
+    if (name === "email") {
+      setEmail(value);
+      setTutorOb((prev) => ({ ...prev, [name]: value }));
+      setOn(true);
     }
-
-    handleEmailChange = (event) => {
-        this.setState({
-            email: event.target.value
-        })
+    if (name === "subject") {
+      setSubject(value);
+      setTutorOb((prev) => ({ ...prev, [name]: value }));
+      setOn(true);
     }
-
-    handleSubjectChange = (event) => {
-        this.setState({
-            subject: event.target.value
-        })
+    if (name === "avail") {
+      setAvail(value);
+      setTutorOb((prev) => ({ ...prev, [name]: value }));
+      setOn(true);
     }
+    console.log(tutorOb);
+  };
 
-    handleAvailChange = (event) => {
-        this.setState({
-            avail: event.target.value
-        })
-    }
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setNewTutors((prev) => [...prev, tutorOb]);
+    setOn((prev) => !prev);
 
-    handleAvailabilityChange = (event) => {
-        this.setState( {
-            availability: true
-        })
-    }
+    console.log(newTutors);
+  };
 
-    onSubmit = e => {
-        e.preventDefault();
-        console.log(this.state);
-    }
+  return (
+    <form>
+      <div>
+        {on ? (
+          " "
+        ) : (
+          <h3>
+            Thanks for being part of change and welcome to our team of tutors.{" "}
+          </h3>
+        )}
+        <label className="Join-text">Name: </label>
+        <input
+          type="text"
+          name="name"
+          placeholder="name"
+          value={name}
+          onChange={handleChange}
+        />
+        <br></br>
+      </div>
 
-    render() {
-        return (
+      <div>
+        <label className="Join-text">Email: </label>
+        <input
+          name="email"
+          type="text"
+          placeholder="email"
+          value={email}
+          onChange={handleChange}
+        />
+        <br></br>
+      </div>
 
-            <form>
-                <div>
-                <label className="Join-text">Name: </label>
-                <input 
-                type="text" 
+      <div>
+        <label className="Join-text">Subject: </label>
+        <select value={subject} onChange={handleChange} name="subject">
+          <option value="">Select</option>
+          <option value="Mathematics">Mathematics</option>
+          <option value="Science">Science</option>
+          <option value="Art">Art</option>
+          <option value="Geography">Geography</option>
+          <option value="English">English</option>
+          <option value="Spanish">Spanish</option>
+        </select>
+      </div>
 
-                placeholder = "name" 
-                value={this.state.name} 
-                onChange = {this.handleNameChange}/>
-                <br></br>
-                </div>
-                
-                <div>
-                <label className="Join-text">Email: </label>
-                <input 
-                type="text" 
-                placeholder = "email" 
-                value={this.state.email}
-                onChange = {this.handleEmailChange}/>
-                <br></br>
-                </div>
+      <div>
+        <label className="Join-text">Availability: </label>
+        <select value={avail} onChange={handleChange} name="avail">
+          <option value="">Select</option>
+          <option value="Mondays">Monday</option>
+          <option value="Tuesdays">Tuesday</option>
+          <option value="Wednesdays">Wednesday</option>
+          <option value="Thursdays">Thursday</option>
+          <option value="Fridays">Friday</option>
+          <option value="Saturdays">Saturday</option>
+          <option value="Sundays">Sunday</option>
+        </select>
+        <br></br>
+      </div>
 
-                <div>
-                    <label className="Join-text">Subject: </label>
-                    <select value = {this.state.subject} onChange={this.handleSubjectChange}>
-                        <option value = "">Select</option>
-                        <option value = "Mathematics">Mathematics</option>
-                        <option value = "Science">Science</option>
-                        <option value = "Art">Art</option>
-                        <option value = "Geography">Geography</option>
-                        <option value = "English">English</option>
-                        <option value = "Spanish">Spanish</option>
-                    </select>
-                </div>
-
-                {/* <div>
-                    <input type="checkbox" value=
-                </div> */}
-
-                <div>
-                    <label className="Join-text">Availability: </label>
-                    <select value = {this.state.avail} onChange={this.handleAvailChange}>
-                        <option value = "">Select</option>
-                        <option value = "Mon">Monday</option>
-                        <option value = "Tue">Tuesday</option>
-                        <option value = "Wed">Wednesday</option>
-                        <option value = "Thu">Thursday</option>
-                        <option value = "Fri">Friday</option>
-                        <option value = "Sat">Saturday</option>
-                        <option value = "Sun">Sunday</option>
-                    </select>
-                    <br></br>
-                </div>
-
-
-                {/* <div>
+      {/* <div>
                     <option value = 
                     <input type="checkbox" checked={this.state.available} onChange={this.handleAvailabilityChange}/>
                 </div> */}
 
-
-                <br></br>
-                <button onClick={e=>this.onSubmit(e)}>Submit</button>
-            </form>
-        );
-    }
-
+      <br></br>
+      <button onClick={onSubmit}>Submit</button>
+    </form>
+  );
 }
 
 export default Form;
